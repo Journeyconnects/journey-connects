8ef491f6e3e446147cba459ba3f9914376e59ce5
Jash <contact@journeyconnects.com>
2026-07-07 00:58:35 -0500

    Remove jsx extensions from all imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
-import Home from './pages/Home.jsx'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
 import './index.css'
 
 function App() {
